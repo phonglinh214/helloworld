8f236adc4650cd47be4a27ad172669707630d340
Tran Minh Quan <quantmgch200753@fpt.edu.vn>
2022-09-22 16:15:41 +0700

    up

---
+++ greenwich.php
@@ -0,0 +1,3 @@
+<?php
+echo "<h1>Chào mừng các bạn tân sinh viên gia nhập
+   ngôi nhà Greenwich Việt Nam </h1>";
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+echo "<h1 style='color: red';>Hello World</h1>";
+echo "<h2 style='color: blue';>Deploy code to Heroku successfully ! </h2>";
