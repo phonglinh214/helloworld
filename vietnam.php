b44dbce652c6c90a176f2274a24057caa5d16ae8
Tran Minh Quan <quantmgch200753@fpt.edu.vn>
2022-09-22 16:52:59 +0700

    ok

--- vietnam.php
+++ vietnam.php
@@ -2,21 +2,32 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale">
-    <title>User Profile</title>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Vietnam</title>
+   <style>
+   body {
+      background-color: lavenderblush;
+   }
+   </style>
+   <link rel="stylesheet" href="css/style.css">
+   <!-- CSS only -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Vietnam is a peaceful country !</h1>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/6zRitbEYe9c" title="YouTube video player"
+   <h1 style="color: red;">Vietnam is a peaceful country !</h1>
+   <h2 class="text text-primary">Hanoi is the capital of Vietnam</h2>
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/6zRitbEYe9c" title="YouTube video player"
       frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen></iframe>
+      allowfullscreen>
+   </iframe>
    <img
       src="https://a.cdn-hotels.com/gdcs/production144/d1394/a304783d-6dc9-4bb2-9239-d124a16a154e.jpg?impolicy=fcrop&w=800&h=533&q=medium"
       alt="">
-    <img src="images/vietnam.png" alt="">
+   <img src="images/vietnam.png" alt="">
 </body>
 
 </html>
