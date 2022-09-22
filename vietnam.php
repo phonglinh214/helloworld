53d656571902fc076b2446b926536ccd3af766fe
Tran Minh Quan <quantmgch200753@fpt.edu.vn>
2022-09-22 16:25:58 +0700

    up1

---
+++ vietnam.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale">
+    <title>User Profile</title>
+</head>
+
+<body>
+   <h1>Vietnam is a peaceful country</h1>
+</body>
+
+</html>
